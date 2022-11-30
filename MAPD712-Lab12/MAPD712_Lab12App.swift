f00f5ac5b51f9121ddbecfe4042f4c55cc36d57e
charrrleee <charlcpy@gmail.com>
2022-11-30 18:38:15 -0500

    Initial Commit

---
+++ MAPD712-Lab12/MAPD712_Lab12App.swift
@@ -0,0 +1,17 @@
+//
+//  MAPD712_Lab12App.swift
+//  MAPD712-Lab12
+//
+//  Created by Charlene Cheung on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MAPD712_Lab12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
